cb1a8fe271b3710f0fbba3f49db071a2a905d36b
Andrey Fedinyak <fedinyakandrey@gmail.com>
2022-06-23 16:45:25 +0300

    Setup environment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Init
+[![Maintainability](https://api.codeclimate.com/v1/badges/6f11a70ee894c9488991/maintainability)](https://codeclimate.com/github/Fedinyak/get-price/maintainability)
+
+[![Node CI](https://github.com/Fedinyak/get-price/workflows/Node%20CI/badge.svg)](https://github.com/Fedinyak/get-price/actions)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  // plugins: [new HtmlWebpackPlugin()],
+};
